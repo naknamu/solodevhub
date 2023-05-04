ee7c9863242b2e91d987896296b66179bf41dfda
naknamu <jodeldelvalle@gmail.com>
2023-05-04 08:48:59 +0800

    Added mobile nav menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,17 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+
+  const handleMenuClick = () => {
+    setMenu(true);
+  }
+
+  const handleCloseClick = () => {
+    setMenu(false);
+  }
+
   return (
     <header className="header">
       <div className="container">
@@ -9,9 +20,29 @@ const Navbar = () => {
           <h1 className="logo-text h1">SoloDevHub</h1>
         </Link>
 
-        <div className="menu-btn">
-          <span className="material-symbols-outlined">menu</span>
+        <div className="nav-btn">
+          <span className="material-symbols-outlined" onClick={() => handleMenuClick()}>menu</span>
         </div>
+
+         
+        <div className={`mobile-nav ${menu ? "active" : ""}`}>
+          <div className="mobile-nav-wrapper">
+
+            <div className="nav-btn closeBtn" onClick={() => handleCloseClick()}>
+              <span className="material-symbols-outlined">
+                close
+              </span>
+            </div>
+
+            <div className="nav-links">
+              <Link to="/" onClick={() => handleCloseClick()}>Home</Link>
+              <Link>Categories</Link>
+              <Link>Tags</Link>
+            </div>
+
+          </div>
+        </div>
+        
       </div>
     </header>
   );
