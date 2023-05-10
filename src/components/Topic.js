407547392182b61af7b50b7074e319b69e1df51a
naknamu <jodeldelvalle@gmail.com>
2023-05-10 17:53:58 +0800

    Added line break and escape character parser

--- src/components/Topic.js
+++ src/components/Topic.js
@@ -1,11 +1,17 @@
 import BlogListCard from "../components/BlogListCard";
 
 const Topic = ({ topic, topicName, blogPosts }) => {
+
+  // Create a new DOMParser
+  const parser = new DOMParser();
+
   return (
     <div className="topic-detail">
       <div className="container">
         <h1 className="topic-title h1">{topic.name}</h1>
-        <p>{topic.detail}</p>
+        <p style={{ whiteSpace: 'pre-line' }}>
+          {parser.parseFromString(topic.detail, 'text/html').documentElement.textContent}
+        </p>
         <h2 className="h2">Blog Posts</h2>
         <p>These are the blog posts under this {topicName}:</p>
         <ul className="blogList-wrapper">
