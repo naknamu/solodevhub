59f1f6bfe3112b4be85fb7ac02014569d97d5ece
naknamu <jodeldelvalle@gmail.com>
2023-05-17 17:44:57 +0800

    Added title to blog URL params

--- src/components/BlogListCard.js
+++ src/components/BlogListCard.js
@@ -1,13 +1,22 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 const { DateTime } = require("luxon");
 
 const BlogListCard = ({ blogPost }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    const urlRegex = /\s/g;
+    const url_title = blogPost.title.toLowerCase().replace(urlRegex, '-');
+
+    navigate(`/posts/${blogPost._id}/${url_title}`);
+  }
+
   return (
     <li className="blogList-card">
-      <div className="blogList-card-wrapper">
-        <Link to={`/posts/${blogPost._id}`} className="blogList-title">
+      <div className="blogList-card-wrapper" onClick={handleClick}>
+        <div className="blogList-title">
           {blogPost.title}
-        </Link>
+        </div>
         <p className="blogList-date">
           Published Date:{" "}
           {DateTime.fromJSDate(new Date(blogPost.publishedDate)).toLocaleString(
