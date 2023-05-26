7b5433735690b5c5b64a22cec34be11aa2e6ff3e
naknamu <jodeldelvalle@gmail.com>
2023-05-26 13:41:42 +0800

    Converted to styled-components

--- src/components/Topic.js
+++ src/components/Topic.js
@@ -1,25 +1,43 @@
+import styled from "styled-components";
 import BlogListCard from "../components/BlogListCard";
 import MarkdownPreview from "./MdPreview";
+
+const TopicDetail = styled.div`
+
+  margin-block: 2rem;
+
+  .container {
+    display: grid;
+    gap: 1rem;
+  }
+
+`;
+
+const BloglistWrapper = styled.ul`
+  display: grid;
+  gap: 1.125rem;
+  margin-bottom: var(--pad-3);
+`;
 
 const Topic = ({ topic, topicName, blogPosts }) => {
 
   return (
-    <div className="topic-detail">
+    <TopicDetail>
       <div className="container">
 
         <h1 className="topic-title h1">{topic.name}</h1>
         <MarkdownPreview markdown={topic.detail} />
         
         <h2 className="h2">Blog Posts</h2>
-        <p>These are the blog posts under this {topicName}:</p>
-        <ul className="blogList-wrapper">
+        <h3 className="h3">These are the blog posts under this {topicName}:</h3>
+        <BloglistWrapper>
           {blogPosts &&
             blogPosts.map((blogPost) => (
               <BlogListCard blogPost={blogPost} key={blogPost._id} />
             ))}
-        </ul>
+        </BloglistWrapper>
       </div>
-    </div>
+    </TopicDetail>
   );
 };
 
