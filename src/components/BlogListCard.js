762448550c0202aa74bfc26baadd8daf513a7348
naknamu <jodeldelvalle@gmail.com>
2023-05-26 13:22:10 +0800

    Converted style to styled-components

--- src/components/BlogListCard.js
+++ src/components/BlogListCard.js
@@ -1,5 +1,37 @@
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 const { DateTime } = require("luxon");
+
+const BlogList = styled.li`
+  background-color: var(--background-secondary);
+  box-shadow: var(--shadow-3);
+  border-radius: var(--radius-6);
+  padding: 12px;
+  cursor: pointer;
+
+  :hover {
+    transform: translateY(-1px);
+    box-shadow: var(--shadow-5);
+  }
+`;
+
+const BlogListWrapper = styled.div`
+  display: grid;
+  gap: 1rem;
+`;
+
+const BlogListTile = styled.div`
+  font-size: var(--fs-4);
+  font-weight: 500;
+
+  :hover {
+    text-decoration: underline;
+  }
+`;
+
+const BlogListDate = styled.div`
+  font-size: var(--fs-5);
+`;
 
 const BlogListCard = ({ blogPost }) => {
   const navigate = useNavigate();
@@ -12,19 +44,19 @@ const BlogListCard = ({ blogPost }) => {
   }
 
   return (
-    <li className="blogList-card" onClick={handleClick}>
-      <div className="blogList-card-wrapper">
-        <div className="blogList-title">
+    <BlogList onClick={handleClick}>
+      <BlogListWrapper>
+        <BlogListTile>
           {blogPost.title}
-        </div>
-        <p className="blogList-date">
+        </BlogListTile>
+        <BlogListDate>
           Published Date:{" "}
           {DateTime.fromJSDate(new Date(blogPost.publishedDate)).toLocaleString(
             DateTime.DATE_MED
           )}
-        </p>
-      </div>
-    </li>
+        </BlogListDate>
+      </BlogListWrapper>
+    </BlogList>
   );
 };
 
